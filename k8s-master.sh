2bdf2ef7bf50d21a1dc6150a57462f109ed37e96
andrey.murzich <andrey.murzich@thomsonreuters.com>
2019-09-03 18:55:20 +0300

    Removed ingress configuration

--- k8s-master.sh
+++ k8s-master.sh
@@ -20,5 +20,3 @@ chmod +x /etc/kubeadm_join_cmd.sh
 
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
-
-kubectl apply -f /vagrant/ingress/ingress.yaml
